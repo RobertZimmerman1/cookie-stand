cbbae54f1efc927daf183f4f74ff7664d8d7c79d
ROBERT ZIMMERMAN <ROBERT.R.ZIMMERMAN@GMAIL.COM>
2021-06-17 19:35:58 -0700

    added constructor function

--- app.js
+++ app.js
@@ -1,118 +1,124 @@
 'use strict';
 
-//global variables for whole site ---------------------------------
-let hoursOfDay = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
+//global variables ---------------------------------------------------------------------------------------------
+let hoursOfDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-//create a constructor function for stores -------------------------------
-function Store(name, minHourlyCustomers, maxHourlyCustomers, avgCookiesCustomer) {
-  this.name = name;
-  this.minHourlyCustomers = minHourlyCustomers;
-  this.maxHourlyCustomers = maxHourlyCustomers;
-  this.avgCookiesCustomer = avgCookiesCustomer;
-  this.hourlyCustomersArray = [];
-  this.hourlyCookieSales = [];
+let salesTableElem = document.getElementById('salesInfo');
+let theadElem = document.createElement('thead');
+salesTableElem.appendChild(theadElem);
+let tbodyElem = document.createElement('tbody');
+salesTableElem.appendChild(tbodyElem);
+let tfootElem = document.createElement('tfoot');
+salesTableElem.appendChild(tfootElem);
 
-  console.log('this', this);
-
-  this.storeArray.push(this);
-  console.log('this is the current version of the array', this.storeArray);
-}
-
-Store.prototype.storeArray = [];
-
-Store.prototype.getHourlyCustomers = function() {
-  this.hourlyCustomers = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-}
-
-//for loops to generate store property arrays -------------------------------------
-Store.prototype.getHourlyCustomersArray = function() {
-  for (let i = 0; i < hoursOfDay.length; i++) {
-    this.getHourlyCustomers();
-    this.hourlyCustomersArray.push(this.hourlyCustomers);
-  }
-}
-
-Store.prototype.getHourlyCookieSales = function() {
-  for (let i = 0; i < this.hourlyCustomersArray.length; i++) {
-    this.hourlyCookieSales.push(Math.floor(this.hourlyCustomersArray[i]*this.avgCookiesCustomer));
-  }
-}
-
-//global function -------------------------------------------------------------
-function renderStores() {
-  for (let i = 0; i < Store.prototype.storeArray.length; i++) {
-    let currentStore = Store.prototype.storeArray[i];
-    currentStore.getHourlyCustomers();
-    currentStore.getHourlyCustomersArray();
-    currentStore.getHourlyCookieSales();
-  }
-}
-
-// // declare stores --------------------------------------------------------
-let seattle = new Store('Seattle', 23, 65, 6.3);
+let seattle =  new Store('Seattle', 23, 65, 6.3);
 let tokyo = new Store('Tokyo', 3, 24, 1.2);
 let dubai = new Store('Dubai', 11, 38, 3.7);
 let paris = new Store('Paris', 20, 38, 2.3);
 let lima = new Store('Lima', 2, 16, 4.6);
 
-//call the function -----------------------------------------------------------
-renderStores();
-
-//table work --------------------------------------------------------------------
-for (let i = 0; i < this.hourlyCookieSales.length; i++) {
-  let tdElem = document.createElement(td);
-  tdElem.textContent = this.hourlyCookieSales[i];
-  trElem.appendChild(tdElem);
-  console.log(tdElem);
+// constructor function ---------------------------------------------------------------------------------------
+function Store(location, minCustomer, maxCustomer, avgCookieSales) {
+  this.location = location;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.avgCookieSales = avgCookieSales;
 }
 
+//prototype methods ------------------------------------------------------------------------------------------
+Store.prototype.stores = [];
+Store.prototype.stores.push(seattle, tokyo, dubai, paris, lima);
+Store.prototype.custPerHour = function() {
+  return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+}
+Store.prototype.createSalesArray = function() {
+  this.hourlySales = [];
+  for (let i = 0; i < hoursOfDay.length; i++) {
+    let sale = Math.floor(this.custPerHour() * this.avgCookieSales);
+    this.hourlySales.push(sale);
+  }
+}
 
+Store.prototype.renderStoreRow = function() {
+  let dailyTotal = 0;
 
-
-
-//first iteration code below ------------------------------------------------------
-// --------------------------------------------------------------------------------
-
-// }
-
-// function randomCustomers(minCustomers, maxCustomers) {
-//   return Math.floor(Math.random() * (maxCustomers - minCustomers) + minCustomers);
-
-// function cookiesSoldPerHour(customers, avgCookiesPerSale) {
-//   return Math.floor(customers * avgCookiesPerSale);
-// }
-
-// //these are the individual store customer minimums and maximums
-// let seattleCustomers = [23, 66];
-// let tokyoCustomers = [3, 24];
-// let dubaiCustomers = [11,38];
-// let parisCustomers = [20,38];
-// let limaCustomers = [2,16];
- 
-
-// let seattle = {
-//   minCustomers: 23,
-//   maxCustomers: 65 + 1,
-//   //I have to artificially increase maxCustomer by one to get the random number generator to range between 2 and 5.
-//   hourlySales: [],
-//   avgCookiesPerSale: 6.3,
-//   customers: randomCustomers(seattleCustomers[0], seattleCustomers[1]),
+  let rowElem = document.createElement('tr');
+  tbodyElem.appendChild(rowElem);
+  let locationThElem = document.createElement('th');
+  locationThElem.textContent = this.location;
+  rowElem.appendChild(locationThElem)
   
+  for (let i = 0; i < this.hourlySales.length; i++) {
+    dailyTotal += this.hourlySales[i];
+    const tdElem = document.createElement('td');
+    tdElem.textContent = this.hourlySales[i];
+    rowElem.appendChild(tdElem);
+  }
+  let tdElemTotal = document.createElement('td');
+  tdElemTotal.textContent = dailyTotal
+  rowElem.appendChild(tdElemTotal);
+}
 
-// }
-// function salesByTheHour() {
-//   for (let i = 0; i > storeHours.length; i++) {
-//     let currentSale = this.customers * this.avgCookiesPerSale;
-//     seattle.hourlySales.push(currentSale);
-    
-//   }
-// }
+// global functions ---------------------------------------------------------------
+function updateStoresWithSales(stores) {
+  for (let i = 0; i < stores.length; i++) {
+    stores[i].createSalesArray();
+  }
+}
 
-// let storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
-// // let storeArray = [seattle, tokyo, dubai, paris, lima];
+function renderHeader() {
+  let rowElem = document.createElement('tr');
+  theadElem.appendChild(rowElem);
+  let blankThElem = document.createElement('th');
+  rowElem.appendChild(blankThElem);
+  
+  for (let i = 0; i < hoursOfDay.length; i++) {
+    let thElem = document.createElement('th');
+    thElem.textContent = hoursOfDay[i];
+    rowElem.appendChild(thElem);
+  }
+  let thElemTotals = document.createElement('th');
+    thElemTotals.textContent = 'Totals'
+    rowElem.appendChild(thElemTotals);
+}
 
-// seattle.salesByTheHour();
-// console.log(seattle);
+function renderAllStoreRows(stores) {
+  for (let i = 0; i < stores.length; i++) {
+    stores[i].renderStoreRow();
+  }
+}
 
-// //my code ends here
+function renderFooter() {
+  let hourlyTotal = 0;
+  let grandTotal = 0;
+  
+  let rowElem = document.createElement('tr');
+  tfootElem.appendChild(rowElem);
+  
+  let blankThElem = document.createElement('th');
+  blankThElem.textContent = 'Totals';
+  rowElem.appendChild(blankThElem);
 
+  for (let i = 0; i < hoursOfDay.length; i++) {
+    for (let j = 0; j < Store.prototype.stores.length; j++) {
+      let currentStoreAtCurrentHour = Store.prototype.stores[j].hourlySales[i];
+      hourlyTotal += currentStoreAtCurrentHour;
+    }
+  
+  let hourlyThElem = document.createElement('th');
+  hourlyThElem.textContent = hourlyTotal;
+  rowElem.appendChild(hourlyThElem);
+  grandTotal += hourlyTotal;
+  hourlyTotal = 0;
+  }
+  let grandThElem = document.createElement('th');
+  grandThElem.textContent = grandTotal;
+  rowElem.appendChild(grandThElem);
+}
+
+// call the functions --------------------------------------------------------------------
+updateStoresWithSales(Store.prototype.stores);
+renderAllStoreRows(Store.prototype.stores);
+renderHeader();
+renderFooter();
+console.log(Store.prototype.stores);
